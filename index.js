90b8c771b0efba370b7144d033a8b7d5bc26fc50
Greg Grigorjevs <ggrigorjevs@icloud.com>
2023-09-09 11:21:15 +0100

    make bionify work on the entire book

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 import * as cheerio from 'cheerio';
-import * as fs from 'fs';
+import fs from 'fs-extra'
+import path from 'path'
+
+function bionifyBookTest(bookName) {
+  const testBook = bookName + ' test.epub'
+  fs.copySync(`${bookName}.epub`, testBook)
+
+  const bookFile = testBook + '/OEBPS/' + fs.readdirSync(testBook + '/OEBPS')[1]
+  console.log(bookFile)
+
+  fs.writeFileSync(bookFile, bionifyFile(bookFile))
+}
+
+function bionifyBook(bookDirectory) {
+  const bookFiles = fs.readdirSync(bookDirectory)
+  for (let bookFile of bookFiles) {
+    bookFile = `${bookDirectory}/${bookFile}`
+    if (path.extname(bookFile) == '.html' || path.extname(bookFile) == '.xhtml') {
+      fs.writeFileSync(bookFile, bionifyFile(bookFile))
+    }
+  }
+}
 
 
-const file = 'The Almanack of Naval Ravikant A Guide to Wealth and Happiness copy.epub/OEBPS/Eric-Jorgenson_The-Almanack-of-Naval-Ravikant_PRODUCTION_v102-3.xhtml'
-const buffer = fs.readFileSync(file);
+function bionifyFile(file) {
+  const buffer = fs.readFileSync(file);
 
-const $ = cheerio.load(buffer);
+  const $ = cheerio.load(buffer, {decodeEntities: false, xmlMode: true, recognizeSelfClosing: true});
 
-const nodes = $('p, li')
+  const nodes = $('p, li')
 
-nodes.each((_i, node) => {
-  node = $(node)
-  node.html(bionifyText(node.text()))
-})
+  nodes.each((_i, node) => {
+    node = $(node)
+    node.html(bionifyText(node.text()))
+  })
+
+  return $.html()
+}
+
 
 function bionifyText(text) {
   return text.split(' ').map(word => bionifyWord(word)).join(' ')
@@ -37,4 +62,4 @@ function bionifyWord(word) {
 
 }
 
-fs.writeFileSync('./test.html', $.html());
+/* fs.writeFileSync('./test.html', $.html()); */
